a9bd91637bb41aa0edfaea5488bc0abfd9d2aa7b
UlisesAguirre <ulises.m.aguirre@hotmail.com>
2023-04-28 14:41:32 -0300

    Add basic project structure

---
+++ restaurant_reservations/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Esta es la pagina de un bar cariñoso</h1>
+      <p>Y como estas? me llego un privado al instagram. Era el amor que alguna vez, estuvo a mi lado pero fueeeee.</p>
+    </div>
+  );
+}
+
+export default App;
