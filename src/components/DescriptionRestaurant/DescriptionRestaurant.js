c7e1cb5c0ffa5a69fcfe9973f7ba39847cfd4976
UlisesAguirre <ulises.m.aguirre@hotmail.com>
2023-05-04 18:10:23 -0300

    Modified: header - navBar - Main

---
+++ src/components/DescriptionRestaurant/DescriptionRestaurant.js
@@ -0,0 +1,13 @@
+
+import "./descriptionRestaurant.css"
+
+const DescriptionRestaurant = () => {
+  return (
+    <div className = "description-container">
+        <h1>Somos autenticos en sabor</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem hic quas fuga expedita ducimus facere ipsa veritatis tenetur nostrum, omnis in sapiente pariatur libero animi, earum deserunt debitis? Beatae hic porro, cum fuga aspernatur assumenda suscipit labore. Ea, minima odit.</p>
+    </div>
+  )
+}
+
+export default DescriptionRestaurant
