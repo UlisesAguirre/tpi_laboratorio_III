9888871c940c0cb887f0270b10369f7d7b85d69f
josedaniele <josedaniele493@gmail.com>
2023-05-15 18:38:26 -0300

    Log in changed to spanish

--- src/components/Form/FormLogIn.js
+++ src/components/Form/FormLogIn.js
@@ -1,31 +1,46 @@
-import React from 'react'
-import "./formLogIn.css"
-import NavBar from "../NavBar/NavBar"
-
+import React from "react";
+import "./formLogIn.css";
+import NavBar from "../NavBar/NavBar";
 
 const FormLogIn = () => {
   return (
     <div>
-        <NavBar/>
-        <div class="LogIn">
-            <div class="form">
-                <h3 class="form-tittle form-all" >Log in</h3>
-                <div class="form-input">
-                    <label class="form-input-label" for="usuario">Email</label>
-                    <input class="form-input-input" type="text" name="usuario" placeholder='example@gmail.com'></input>
-                </div>
-                <div class="form-input">
-                    <label class="form-input-label" for="usuario">Password</label>
-                    <input class="form-input-input" type="password" name="usuario" placeholder='**************'></input>
-                    <a href="#" class="form-input-link" >Forget your paswword ?</a>
-                </div>
-                <div>
-                    <button class="form-button" >Log-In</button>
-                </div>
-            </div>
+      <NavBar />
+      <div class="LogIn">
+        <div class="form">
+          <h3 class="form-tittle form-all">Iniciar Sesión</h3>
+          <div class="form-input">
+            <label class="form-input-label" for="usuario">
+              Email
+            </label>
+            <input
+              class="form-input-input"
+              type="text"
+              name="usuario"
+              placeholder="example@gmail.com"
+            ></input>
+          </div>
+          <div class="form-input">
+            <label class="form-input-label" for="usuario">
+              Password
+            </label>
+            <input
+              class="form-input-input"
+              type="password"
+              name="usuario"
+              placeholder="**************"
+            ></input>
+            <a href="#" class="form-input-link">
+              Olvidaste tu contraseña?
+            </a>
+          </div>
+          <div>
+            <button class="form-button">Iniciar sesión</button>
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FormLogIn
+export default FormLogIn;
