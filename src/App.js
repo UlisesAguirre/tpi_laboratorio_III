7ebfd6090283df9d23fdffad2db0431bb2a9822d
josedaniele <josedaniele493@gmail.com>
2023-05-23 16:37:07 -0300

    fixing problems with the routes and files of tomi pr

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Header from "./components/Header/Header"
 import Main from "./components/Main/Main"
 import Footer from "./components/Footer/Footer"
-import FormLogIn from "./components/FormLogIn/FormLogIn"
+import FormLogIn from "./components/Form/FormLogIn/FormLogIn"
 import Comments from "./components/Comments/Comments"
 import AboutUs from "./components/AboutUs/AboutUs"
+import FormRegister from "./components/Form/FormRegister/FormRegister"
 
 import { Route, Routes } from 'react-router-dom';
 
@@ -18,6 +19,7 @@ function App() {
       <Routes>
         <Route path='/' element= {<Main/>} />
         <Route path='/login' element= {<FormLogIn/>} />
+        <Route path='/register' element= {<FormRegister/>} />
         <Route path="/comments" element = {<Comments/>} />
         <Route path="/about-us" element = {<AboutUs/>} />
       </Routes>
