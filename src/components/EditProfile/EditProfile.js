ee065c6c43cbe82f9b313df68b34cdd529aa70df
UlisesAguirre <ulises.m.aguirre@hotmail.com>
2023-06-12 19:37:50 -0300

    change

--- src/components/EditProfile/EditProfile.js
+++ src/components/EditProfile/EditProfile.js
@@ -3,24 +3,16 @@ import ClientMenu from "../ClientMenu/ClientMenu"
 
 import "./editProfile.css"
 import ProfileDataView from "../ProfileDataView/ProfileDataView"
-import { useState } from "react"
+import { useContext, useState } from "react"
 import ListUser from "../ListUser/ListUser"
+import UserContext from "../Context/UserContext"
 
 
 
 const EditProfile = ({edit}) => {
   const [editProfile, setEditProfile] = useState(edit);
 
-  const client = {
-    id: 1,
-    icon: "https://static.vecteezy.com/system/resources/thumbnails/005/545/335/small/user-sign-icon-person-symbol-human-avatar-isolated-on-white-backogrund-vector.jpg",
-    name: "Tito",
-    lastName: "Fuentes",
-    phone: "3416476578",
-    email: "tugrp@example.com",
-    password: "123456",
-    role: "client", //Ir variando entre los roles de admin, client y superAdmin para ver las diferencias. (userContext)
-  }
+  const user = useContext(UserContext);
 
   const editProfileHandler = () => {
     setEditProfile(true);
@@ -28,14 +20,14 @@ const EditProfile = ({edit}) => {
 
   return (
     <div className="client-container">
-      <ClientMenu client={client} />
+      <ClientMenu />
       <div className="editProfile-container">
-        {client.role === "client"? (
+        {user.role === "client"? (
           editProfile ?
-            (<Form title={"Editar perfil"} buttonTitle={"Guardar"} link={"/edit-profile"} data={client} register={false} />)
-            : (<ProfileDataView user={client} editProfile={editProfileHandler} />)
+            (<Form title={"Editar perfil"} buttonTitle={"Guardar"} link={"/edit-profile"} data={user} register={false} />)
+            : (<ProfileDataView user={user} editProfile={editProfileHandler} />)
           
-        ): client.role === "admin"? <ListUser typeUser={"clients"} /> : <ListUser typeUser={"users"}/>}
+        ): user.role === "admin"? <ListUser typeUser={"clients"} /> : <ListUser typeUser={"users"}/>}
       </div>
     </div>
   )
